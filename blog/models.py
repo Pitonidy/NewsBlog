a0bf5c3d7047fe052e90a90d4dc225a2eb359097
kiri3914 <t.bektur.2002@gmail.com>
2022-12-07 14:06:49 +0600

    add News

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    description = models.TextField(verbose_name="Текст", null=True, blank=True)
+    image = models.ImageField(upload_to='news/',
+                              verbose_name="Изображение", null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               verbose_name="Автор")
+    created_at = models.DateTimeField(
+                                      verbose_name="Дата создания")
+    data = models.DateField()
+    time = models.TimeField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['created_at']
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
